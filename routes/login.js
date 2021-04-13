822dadf7e24f05e1b4e13aaba9fb3809e839ec16
YongjinKim <dydwls0669@gmail.com>
2021-04-13 14:11:38 +0900

    Normal SignUp

--- routes/login.js
+++ routes/login.js
@@ -9,11 +9,9 @@ router.get("/", async (req, res, next) => {
 });
 
 // 어플 내에서의 일반 로그인
-router.post("/normal", async (req, res) => {  
-});
+router.post("/normal", async (req, res) => {});
 
 // 소셜 로그인
-router.post("/social", async (req, res, next) => {
-});
+router.post("/social", async (req, res, next) => {});
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -6,7 +6,7 @@ const router = express.Router();
 var admin = require("firebase-admin");
 var serviceAccount = require("../butterfly-efb30-firebase-adminsdk-2x0u2-344c142e5a.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 router.get("/", async (req, res, next) => {
@@ -14,12 +14,38 @@ router.get("/", async (req, res, next) => {
   res.send("signup Server");
 });
 
+router.post("/", async (req, res) => {
+  var data = req.body;
+  console.log(data);
+  res.send(data);
+});
+
 router.post("/normal", async (req, res) => {
+  var data = req.body;
+  console.log("/normal");
 
+  const user = new User({
+    email: data.email,
+    password: crypto
+      .createHash("sha512")
+      .update(data.password)
+      .digest("base64"),
+    phone_num: data.phone,
+    nickname: data.nickname,
+    category: data.category,
+  });
+  console.log("Here");
+  user.save((err) => {
+    if (err) {
+      console.log("/normal : Save Error");
+      res.status(200).send({ save: "fail" });
+    } else {
+      console.log("/normal : Save Success");
+      res.status(500).send({ save: "success" });
+    }
+  });
 });
 
-router.post("/social", async (req, res, next) => {
- 
-});
+router.post("/social", async (req, res, next) => {});
 
 module.exports = router;
