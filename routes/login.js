313d5273ac8d345ecf3c7b9e2f771d6d406272cd
KHS-kr <ks96ks@ajou.ac.kr>
2021-04-13 13:35:48 +0900

    add normal login

--- routes/login.js
+++ routes/login.js
@@ -9,7 +9,26 @@ router.get("/", async (req, res, next) => {
 });
 
 // 어플 내에서의 일반 로그인
-router.post("/normal", async (req, res) => {  
+router.post("/normal", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(req.body);
+  const findUser = User.findOne({
+    email: email,
+    password: password
+  });
+
+  findUser.getFilter();
+
+  const result = await findUser.exec();
+
+  console.log(result);
+  const response = {};
+  response["result"] = result != null ? "SUCCESS" : "FAIL";
+  response["userId"] = result != null ? 1 : 0;
+
+  console.log("POST : /login/normal");
+  res.send(response);
 });
 
 // 소셜 로그인
