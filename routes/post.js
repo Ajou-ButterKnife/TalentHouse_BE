b7aae475f69cc2e58630062ec5634a837c6f3876
YongjinKim <dydwls0669@gmail.com>
2021-04-25 17:13:54 +0900

    modify

--- routes/post.js
+++ routes/post.js
@@ -3,14 +3,33 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  console.log("GET :/signup");
-  res.send("signup Server");
+  const posts = await Post.find({});
+  console.log("GET Post");
+  const retval = {
+    data: posts,
+  };
+  res.status(200).send(retval);
 });
 
-router.post("/", async (req, res) => {
+router.post("/create", async (req, res) => {
   var data = req.body;
-  console.log(data);
-  res.send(data);
+
+  const post = new Post({
+    writer_id: data.writerId,
+    writer_nickname: data.writerNickname,
+    title: data.title,
+    category: data.category,
+    description: data.description,
+  });
+  post.save((err) => {
+    if (err) {
+      console.log("/create : Save Error");
+      res.status(500).send({ result: "Fail" });
+    } else {
+      console.log("/create : Save Success");
+      res.status(200).send({ result: "Success" });
+    }
+  });
 });
 
 module.exports = router;
