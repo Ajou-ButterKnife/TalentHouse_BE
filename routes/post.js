b0f88961de6751565f3881a3ee17f71be16e135b
GodSang <tkdgh584@gmail.com>
2021-05-01 14:19:54 +0900

    게시글 업로드 수정

--- routes/post.js
+++ routes/post.js
@@ -1,38 +1,17 @@
-const Post = require("../models/post");
-const express = require("express");
+const Post = require('../models/post');
+const express = require('express');
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const posts = await Post.find({});
-  console.log("GET Post");
+  console.log('GET Post');
   const retval = {
     data: posts,
   };
   res.status(200).send(retval);
 });
 
-router.post("/create", async (req, res) => {
-  var data = req.body;
-
-  const post = new Post({
-    writer_id: data.writerId,
-    writer_nickname: data.writerNickname,
-    title: data.title,
-    category: data.category,
-    description: data.description,
-  });
-  post.save((err) => {
-    if (err) {
-      console.log("/create : Save Error");
-      res.status(500).send({ result: "Fail" });
-    } else {
-      console.log("/create : Save Success");
-      res.status(200).send({ result: "Success" });
-    }
-  });
-});
-
-router.post("/create", async (req, res) => {
+router.post('/create', async (req, res) => {
   var data = req.body;
 
   const post = new Post({
@@ -44,10 +23,10 @@ router.post("/create", async (req, res) => {
     video_url: data.videoUrl,
   });
   post.save((err) => {
-    if(err) {
-      res.status(200).json({ result: "Fail"});
+    if (err) {
+      res.status(200).json({ result: 'Fail' });
     } else {
-      res.status(200).json({ result: "Success"});
+      res.status(200).json({ result: 'Success' });
     }
   });
 });
