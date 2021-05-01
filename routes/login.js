f16a52988e2812656187991d00e155b5f596c287
GodSang <tkdgh584@gmail.com>
2021-05-01 18:53:59 +0900

    prettier 사용한 줄 정렬

--- routes/login.js
+++ routes/login.js
@@ -29,15 +29,19 @@ router.post('/normal', async (req, res) => {
 
   const response = {};
 
-  response["result"] = result != null ? "Success" : "Fail";
-  response["detail"] = result != null ? "" : "아이디 혹은 비밀번호가 잘못되었습니다.";
-  response["data"] = result != null ? {
-    "_id" : result._id,
-    "nickname" : result.nickname,
-  } : null;
-  console.log("response")
-  console.log(response)
-  console.log()
+  response['result'] = result != null ? 'Success' : 'Fail';
+  response['detail'] =
+    result != null ? '' : '아이디 혹은 비밀번호가 잘못되었습니다.';
+  response['data'] =
+    result != null
+      ? {
+          _id: result._id,
+          nickname: result.nickname,
+        }
+      : null;
+  console.log('response');
+  console.log(response);
+  console.log();
   res.status(200).send(response);
 });
 
@@ -56,12 +60,10 @@ router.post('/social', async (req, res, next) => {
         // 로그인 성공, DB에 동일 uid 존재
         var unique_id = user_dt._id;
         console.log(unique_id);
-        res
-          .status(200)
-          .json({
-            result: 'Success',
-            data: { _id: unique_id, nickname: user_dt.nickname },
-          });
+        res.status(200).json({
+          result: 'Success',
+          data: { _id: unique_id, nickname: user_dt.nickname },
+        });
       }
     }
   });
--- routes/signup.js
+++ routes/signup.js
@@ -1,51 +1,51 @@
-const User = require("../models/user");
-const express = require("express");
-const crypto = require("crypto");
+const User = require('../models/user');
+const express = require('express');
+const crypto = require('crypto');
 const router = express.Router();
 
-var admin = require("firebase-admin");
-var serviceAccount = require("../butterfly-efb30-firebase-adminsdk-2x0u2-344c142e5a.json");
+var admin = require('firebase-admin');
+var serviceAccount = require('../butterfly-efb30-firebase-adminsdk-2x0u2-344c142e5a.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-router.get("/", async (req, res, next) => {
-  console.log("GET :/signup");
-  res.send("signup Server");
+router.get('/', async (req, res, next) => {
+  console.log('GET :/signup');
+  res.send('signup Server');
 });
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   var data = req.body;
   console.log(data);
   res.send(data);
 });
 
-router.post("/normal", async (req, res) => {
+router.post('/normal', async (req, res) => {
   var data = req.body;
-  console.log("/normal");
+  console.log('/normal');
 
   const user = new User({
     email: data.email,
     password: crypto
-      .createHash("sha512")
+      .createHash('sha512')
       .update(data.password)
-      .digest("base64"),
+      .digest('base64'),
     phone_num: data.phone,
     nickname: data.nickname,
     category: data.category,
   });
   user.save((err) => {
     if (err) {
-      console.log("/normal : Save Error");
-      res.status(500).send({ result: "Fail" });
+      console.log('/normal : Save Error');
+      res.status(500).send({ result: 'Fail' });
     } else {
-      console.log("/normal : Save Success");
-      res.status(200).send({ result: "Success" });
+      console.log('/normal : Save Success');
+      res.status(200).send({ result: 'Success' });
     }
   });
 });
 
-router.post("/social", async (req, res, next) => {
+router.post('/social', async (req, res, next) => {
   var data = req.body;
 
   var userInfo = await admin.auth().getUser(data.uid);
@@ -59,14 +59,14 @@ router.post("/social", async (req, res, next) => {
   user.save((err) => {
     if (err) {
       //console.log(err);
-      res.status(200).json({ result: "Fail" });
+      res.status(200).json({ result: 'Fail' });
     } else {
-      res.status(200).json({ result: "Success" });
+      res.status(200).json({ result: 'Success' });
     }
   });
 });
 
-router.post("/nickname_overlap_check", async (req, res, next) => {
+router.post('/nickname_overlap_check', async (req, res, next) => {
   const user_nickname = req.body.nickname;
   const user_check = await User.findOne(
     { nickname: user_nickname },
@@ -76,17 +76,17 @@ router.post("/nickname_overlap_check", async (req, res, next) => {
       } else {
         if (check_dt == null) {
           // 중복되는 닉네임 없음
-          res.status(200).json({ result: "Success" });
+          res.status(200).json({ result: 'Success' });
         } else {
           // 중복되는 닉네임 존재
-          res.status(200).json({ result: "Fail" });
+          res.status(200).json({ result: 'Fail' });
         }
       }
     }
   );
 });
 
-router.post("/email_overlap_check", async (req, res, next) => {
+router.post('/email_overlap_check', async (req, res, next) => {
   const user_email = req.body.email;
   const user_check = await User.findOne(
     { email: user_email },
@@ -96,10 +96,10 @@ router.post("/email_overlap_check", async (req, res, next) => {
       } else {
         if (check_dt == null) {
           // 중복되는 이메일 없음
-          res.status(200).json({ result: "Success" });
+          res.status(200).json({ result: 'Success' });
         } else {
           // 중복되는 이메일 존재
-          res.status(200).json({ result: "Fail" });
+          res.status(200).json({ result: 'Fail' });
         }
       }
     }
