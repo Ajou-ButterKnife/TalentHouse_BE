3402c6a351a082ce452a76ba7e86ee474b3eb039
GodSang <tkdgh584@gmail.com>
2021-05-21 19:08:49 +0900

    fcm 메소드 수정

--- util.js
+++ util.js
@@ -1,34 +1,23 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
 const value = {};
 
-value.sendFcm = async function(title, body, fbTokenList) {
-    const fcmMessage = {
-        data : {
-            title: title,
-            body: body,
-        },
-        // data : {
-        //
-        // },
-        tokens: fbTokenList,
-    };
-    try {
-        const result = await admin.messaging().sendMulticast(fcmMessage);
-
-        for(let i = 0; i < result.responses.length; i++) {
-            if(!result.responses[i].success) {
-                console.log(result.responses[i].error);
-            }
-        }
-        console.log('Send FCM Success');
-
-        return true;
-    }
-    catch(exception) {
-        console.log(exception);
-        return false;
-    }
+value.sendFcm = async function (title, body, fbTokenList) {
+  const fcmMessage = {
+    data: {
+      title: title,
+      body: body,
+    },
+    token: fbTokenList,
+  };
+  try {
+    const result = await admin.messaging().send(fcmMessage);
+    console.log('Send FCM Success');
+    return true;
+  } catch (exception) {
+    console.log(exception);
+    return false;
+  }
 };
 
 module.exports = value;
